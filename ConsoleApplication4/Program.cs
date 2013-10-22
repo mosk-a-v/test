85a164e38530b1a773051fd398c288f906260f6b
mosk-a-v <Mosk_A_V@mail.ru>
2013-10-22 23:30:20 +0400

    commit2

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApplication4 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("test");
         } 
     }
 }
